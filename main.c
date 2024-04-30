843ab1d76c5a47561a0a5df54b398f1e933b8f2f
Triomph <worker13prime@gmail.com>
2024-04-30 18:33:52 +0200

    Début de script de téléchargement local des dépendances pour gtk3

--- main.c
+++ main.c
@@ -53,7 +53,140 @@ ${workspaceFolder}/**
 /usr/include/cairo
 /usr/include/gdk-pixbuf-2.0
 /usr/include/atk-1.0
+
+
+
+
+
+ÉTAPE PRÉLIMINAIRE
+Télécharger m4            et le mettre dans dependancies : https://ftp.gnu.org/gnu/m4/
+Télécharger autoconf      et le mettre dans dependancies : http://ftp.gnu.org/gnu/autoconf/
+Télécharger automake 1.13 et le mettre dans dependancies : https://ftp.gnu.org/gnu/automake/
+Télécharger libtool       et le mettre dans dependancies : https://mirror.cyberbits.eu/gnu/libtool/
+Télécharger libffi        et le mettre dans dependancies : https://github.com/libffi/libffi/releases/download/v3.4.5/libffi-3.4.5.tar.gz
+Télécharger gettext       et le mettre dans dependancies : https://ftp.gnu.org/pub/gnu/gettext/gettext-0.22.5.tar.gz
+Télécharger glib          et le mettre dans dependancies : https://download.gnome.org/sources/glib/2.9/glib-2.9.6.tar.gz
+
+git clone https://github.com/madler/zlib
+
+
+
+
+SCRIPT:
+
+mkdir dependancies;
+cd dependancies;
+    mkdir zlib_installed;
+    cd zlib;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/zlib_installed;
+    make;
+    make install;
+    export CFLAGS="-I$HOME/Documents/ProjetSDSE_GUI/dependancies/zlib_installed/include $CFLAGS";
+    export LDFLAGS="-L$HOME/Documents/ProjetSDSE_GUI/dependancies/zlib_installed/lib $LDFLAGS";
+    export PKG_CONFIG_PATH="$HOME/Documents/ProjetSDSE_GUI/dependancies/zlib_installed/lib/pkgconfig:$PKG_CONFIG_PATH";
+    cd ..;
+
     
+
+
+    mkdir m4-1.4.19_installed;
+    cd m4-1.4.19;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/m4-1.4.19_installed;
+    make;
+    make install;
+    export PATH=$HOME/Documents/ProjetSDSE_GUI/dependancies/m4-1.4.19_installed/bin:$PATH;
+    cd ..;
+
+    mkdir autoconf-2.72_installed;
+    cd autoconf-2.72;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/autoconf-2.72_installed;
+    make;
+    make install;
+    export PATH=$HOME/Documents/ProjetSDSE_GUI/dependancies/autoconf-2.72_installed/bin:$PATH;
+    cd ..;
+
+    mkdir automake-1.13_installed;
+    cd automake-1.13;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/automake-1.13_installed;
+    make;
+    make install;
+    export PATH=$HOME/Documents/ProjetSDSE_GUI/dependancies/automake-1.13_installed/bin:$PATH;
+    cd ..;
+
+    mkdir libtool-2.4.7_installed;
+    cd libtool-2.4.7;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/libtool-2.4.7_installed;
+    make;
+    make install;
+    export PATH=$HOME/Documents/ProjetSDSE_GUI/dependancies/libtool-2.4.7_installed/bin:$PATH;
+    cd ..;
+
+
+    mkdir libffi-3.4.5_installed;
+    cd libffi-3.4.5;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/libffi-3.4.5_installed;
+    make;
+    make install;
+    export LIBFFI_CFLAGS="-I$HOME/Documents/ProjetSDSE_GUI/dependancies/libffi_installed/include";
+    export LIBFFI_LIBS="-L$HOME/Documents/ProjetSDSE_GUI/dependancies/libffi_installed/lib -lffi";
+    cd ..;
+
+    mkdir gettext-0.22.5_installed;
+    cd gettext-0.22.5;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/gettext-0.22.5_installed;
+    make;
+    make install;
+    export PATH=$HOME/Documents/ProjetSDSE_GUI/dependancies/gettext-0.22.5_installed/bin:$PATH;
+    cd ..;
+
+
+    mkdir glib-2.9.6_installed;
+    cd glib-2.9.6;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/glib-2.9.6_installed;
+    make;
+
+
+
+
+
+    git clone https://github.com/gooroom/gtk3;
+    mkdir gtk3_installed;
+    cd gtk3;
+    ./configure --prefix=$HOME/Documents/ProjetSDSE_GUI/dependancies/gtk3_installed;
+
+
+ÉTAPE Annexe - Vérifier les packages dans l'ordre
+    zlib
+
+    m4
+    automake 1.13
+    autoconf
+
+    libffi
+
+    gettext
+
+    glib-2.0
+
+    atk
+
+    pango
+
+    fribidi
+
+    cairo
+
+    cairo-gobject
+
+    gdk-pixbuf-2.0
+
+    gtk3
+
+
+            
+    
+
+
 */
 
 
